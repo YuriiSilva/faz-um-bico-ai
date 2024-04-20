4a101824a56cb8cb1207887854354e81fa9e3350
rafael <darakrex@gmail.com>
2024-04-20 11:38:23 -0300

    UserCreation

---
+++ app/Enums/UserEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\Enums;
+
+enum userType: int
+ {
+    case UserAdmin = 1;
+    case UserFreelancer = 2;
+    case UHirer = 3 ;
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,10 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'nickname' => fake()->name(),
+            'document' => fake()->cpf(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            
         ];
     }
 
