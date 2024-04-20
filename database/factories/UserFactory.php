434e2ece8c8926ab7779be774e3499a00403b13c
rafael <darakrex@gmail.com>
2024-04-20 12:37:48 -0300

    atzUsers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'nickname' => fake()->name(),
-            'document' => fake()->cpf(),
+            'document' => fake()->uuid(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
